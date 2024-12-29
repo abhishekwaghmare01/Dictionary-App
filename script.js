d69b952bf1ccc9e84d935f081f71cc3db97ae349
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2024-12-29 19:54:31 +0530

    Add read button

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ form.addEventListener('submit', (e) => {
 });
 
 const getWordInfo = async (word) => {
+    try{
     const responce = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     const data = await responce.json();
 
@@ -14,12 +15,12 @@ const getWordInfo = async (word) => {
     
     resultDev.innerHTML = `
         <h2><strong>Word : </strong>${data[0].word}</h2>
-        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p class= "partOfSpeech">${data[0].meanings[0].partOfSpeech}</p>
         <p><strong>Meaning : </strong>${definitions.definition === undefined ? "Not Found" : definitions.definition}</p>
         <p><strong>Example : </strong>${definitions.example === undefined ? "Not Found" : definitions.example}</p>
         <p><strong>Antonyms : </strong></p>
     `;
-
+    //Fetching Antonyms
     if(definitions.antonyms.length === 0){
         resultDev.innerHTML += `<span>Not Found</span>`;
     }
@@ -28,7 +29,13 @@ const getWordInfo = async (word) => {
             resultDev.innerHTML += `<li>${definitions.antonyms[i]}</li>`;
         }
     }
+    //Add read more button
+    resultDev.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More...</a></div>`
 
+}
+catch (error){
+    resultDev.innerHTML = `<p>Sorry, the word could not be found. </p>`;
+}
     
     console.log(data);
 
